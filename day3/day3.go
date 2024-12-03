82b2b0bca81e329bcb92dd3ceb730bff870074ef
Ritwik <sritwik03@gmail.com>
2024-12-04 00:57:20 +0530

    day3

--- day3/day3.go
+++ day3/day3.go
@@ -4,6 +4,8 @@ import (
 	"aoc2024/utils"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -14,24 +16,36 @@ func DAY3() {
 		os.Exit(1)
 	}
 
-	var ans int = 0
-	for _, line := range lines {
-		ans += findMuls(line)
-	}
+	fmt.Printf("Part 1 : %d\n", partOne(lines))
+	fmt.Printf("Part 2 : %d\n", partTwo(lines))
 }
 
-func findMuls(line string) int {
+func partOne(lines []string) int {
+	var ans int = 0
+	for _, line := range lines {
+		ans += findMuls(line, false)
+	}
+
+	return ans
+}
+
+func findMuls(line string, enableCheck bool) int {
 	ans := 0
-	pos := 0
 	indArr := make([]int, 0)
-	for {
-		ind := strings.Index(line[pos:], "mul(")
-		if ind == -1 {
-			break
+	isEnabled := true
+	for i := 0; i < len(line); i++ {
+		if isEnabled && i < len(line)-4 && line[i:i+4] == "mul(" {
+			indArr = append(indArr, i+3)
+			i += 3
 		}
 
-		indArr = append(indArr, ind+pos+3)
-		pos += ind + 4
+		if enableCheck && i < len(line)-4 && line[i:i+4] == "do()" {
+			isEnabled = true
+			i += 3
+		} else if enableCheck && i < len(line)-7 && line[i:i+7] == "don't()" {
+			isEnabled = false
+			i += 6
+		}
 	}
 
 	for i, ind := range indArr {
@@ -39,19 +53,46 @@ func findMuls(line string) int {
 		if nextInd == -1 {
 			break
 		}
-		
+
 		if i < len(indArr)-1 && ind+nextInd > indArr[i+1] {
 			continue
 		}
-		
-		ans 
+
+		// fmt.Println(line[ind+1 : ind+nextInd])
+		ans += calcMul(line[ind+1 : ind+nextInd])
 	}
-	// fmt.Println(indArr)
+
 	return ans
 }
 
 func calcMul(line string) int {
 
-	fmt.Println(line)
+	reg, err := regexp.Compile(`(\d{1,3}),(\d{1,3})`)
+	if err != nil {
+		fmt.Printf("Error matching regex : %v", err)
+		return 0
+	}
+
+	if reg.MatchString(line) {
+		x, y, ok := strings.Cut(line, ",")
+		if !ok {
+			fmt.Printf(", not found in %s\n", line)
+			return 0
+		}
+
+		x_int, _ := strconv.Atoi(x)
+		y_int, _ := strconv.Atoi(y)
+
+		return x_int * y_int
+	}
 	return 0
+}
+
+func partTwo(lines []string) int {
+	var ans int = 0
+	for _, line := range lines {
+		ans += findMuls(line, true)
+	}
+
+	return ans
 }
